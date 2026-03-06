4be7b8e4d633074967e7c4fed0b928a270cdd99c
sabs-script <scriptdark37@gmail.com>
2026-03-06 09:48:22 +0800

    Update lua.lua

--- lua.lua
+++ lua.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://pastefy.app/qiw8w3Nt/raw"))()
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/tb8uXgk6pK/0"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/script-luaau/uiui/refs/heads/main/lua"))()
